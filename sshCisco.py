f839d18c16cedc105ceff5b3828f44bd76c1265f
krazykeef13 <S3285449@students.fscj.edu>
2023-07-08 16:24:02 -0700

    Moving file sshCisco.py up to github

---
+++ sshCisco.py
@@ -0,0 +1,24 @@
+
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+
+
+USERNAME = input("Please enter your username: ")
+PASS = getpass("Please enter your password: ")
+
+device = {
+    'host': '192.168.44.3',
+    'username': 'USERNAME',
+    'password': 'PASS',
+    'device_type': 'cisco_ios'
+}
+
+c = ConnectHandler(**device)
+
+output = c.send_command('show run')
+
+f = open('backup.conf', 'x')
+
+f.write(output)
+f.close()
